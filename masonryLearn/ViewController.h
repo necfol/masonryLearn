7b67695eb251ccf768122a578701451ba1ee16c5
necfol <necfoll@gmail.com>
2017-08-06 20:19:27 +0800

    init proj

---
+++ masonryLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  masonryLearn
+//
+//  Created by necfol on 2017/8/6.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ masonryLearn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  masonryLearn
+//
+//  Created by necfol on 2017/8/6.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
