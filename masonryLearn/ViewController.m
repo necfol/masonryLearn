dcc5a418336c6451486a300531e5fd2530946f7b
necfol <necfoll@gmail.com>
2017-08-09 21:56:17 +0800

    两个view加约束

--- masonryLearn/ViewController.m
+++ masonryLearn/ViewController.m
@@ -19,7 +19,10 @@
     [super viewDidLoad];
     UIView *view = [[UIView alloc] init];
     view.backgroundColor = [UIColor orangeColor];
+    UIView *redView = [UIView new];
+    redView.backgroundColor = [UIColor redColor];
     [self.view addSubview:view];
+    [self.view addSubview:redView];
 //    创建约束
     [view mas_makeConstraints:^(MASConstraintMaker *make) {
 ////        位置约束
@@ -34,7 +37,17 @@
 //        make.left.equalTo(self.view).offset(50);
 //        make.top.equalTo(self.view).offset(50);
 //        make.bottom.equalTo(self.view).offset(-50);
-        make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(50, 50, 50, 50));
+//        make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(50, 50, 50, 50));
+        make.left.mas_equalTo(self.view).offset(20);
+        make.right.mas_equalTo(redView.mas_left).offset(-20);
+        make.bottom.mas_equalTo(self.view).offset(-20);
+        make.height.mas_equalTo(50);
+    }];
+    [redView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(self.view).offset(-20);
+        make.top.mas_equalTo(view.mas_top);
+        make.bottom.mas_equalTo(view.mas_bottom);
+        make.width.mas_equalTo(view.mas_width);
     }];
     
 }
